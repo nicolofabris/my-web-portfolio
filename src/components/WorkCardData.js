5ede4367540548493d4ebc7997165f7d25a76adc
nicolofabris <nicolo.fabriss@gmail.com>
2022-11-27 12:53:30 +0100

    footer and new project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,11 +1,18 @@
 import bg3 from '../assets/bg3.png'
+import bg4 from '../assets/bg4.png'
 
 const ProjectCardData = [
     {
         imgsrc: bg3,
-        title: 'AsOne Ecommerce Website',
-        text: 'Inspired by a need for a sustainable trading place, this community-based online rental space  was thought by me and other 3 colleagues as a "green" replacement for the most famous E-marketplaces. We all worked on both front and back-end aspects of the whole project.',
+        title: 'AsOne Ecommerce',
+        text: 'Inspired by a need for a sustainable trading place, this community-based online rental space  was thought by me and other 3 colleagues as a "green" replacement for the most famous E-marketplaces. The main challenges were appying Redux to manage the reducers and socket.io for the creation of the chat.  ',
         source: 'https://github.com/vanvand/MERN_eCommerce_finalProject'
+    },
+    {
+        imgsrc: bg4,
+        title: 'ReactMeals Food Order App',
+        text: 'I built this simple React application as practice on React states and context. The aim was to have a fully-working cart and product update solely built with React.',
+        source: 'https://github.com/nicolofabris/food-order'
     }
 ]
 export default ProjectCardData
